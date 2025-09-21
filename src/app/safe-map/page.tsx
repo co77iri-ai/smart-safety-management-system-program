ed5bed2a36778e050498de2f974aa92a56120f11
Kang Minsu (BetaMan) <skymin0417@gmail.com>
2025-09-22 05:10:06 +0900

    ave

--- src/app/safe-map/page.tsx
+++ src/app/safe-map/page.tsx
@@ -1,6 +1,8 @@
 import { getContracts, getSiteByContractIds } from "@/models";
 import { SafeMapScreen } from "./components";
 
+export const dynamic = "force-dynamic";
+
 export default async function SafeMap() {
   const contracts = await getContracts();
   const contractSites = await getSiteByContractIds(
